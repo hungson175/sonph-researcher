c1c3ef49bb43e2fd59c9539cb5bf48ccdd2b7b55
hungson175 <sphamhung@gmail.com>
2024-09-26 19:35:29 +0700

    fix: added next.js

---
+++ components/example.js
@@ -0,0 +1,12 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function Example() {
+  return (
+    <div>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+      <p>Example with Heroicon</p>
+    </div>
+  )
+}
+
+export default Example
